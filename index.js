5a2bab53dcc198cf28369db6978d1b5f2fc9c7ba
ARTHUR MARTINS <arthurmvo@gmail.com>
2025-03-15 08:38:47 -0300

    v3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { exec } = require('child_process');
 const OS = process.env.CURRENT_OS;
 const streamers = process.env.STREAMERS.split(',');
 
-// Your Twitch client credentials
+// Twitch API Credentials
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
@@ -21,42 +21,34 @@ async function getAccessToken() {
       },
     });
 
-    return response.data.access_token;  // The Bearer token
+    return response.data.access_token; // The Bearer token
   } catch (error) {
     console.error('Error getting access token:', error);
-    process.exit(1);  // Exit if unable to get token
+    process.exit(1); // Exit if unable to get token
   }
 }
 
 // Function to open Twitch streams in a browser
-function openBrowserOnStreams() {
-  streamers.forEach(async (streamer) => {
-    const statusFile = `${streamer}.txt`;
-    let previousStatus = fs.existsSync(statusFile) ? fs.readFileSync(statusFile, 'utf8') : '';
+async function openBrowserOnStreams() {
+  const token = await getAccessToken(); // Get OAuth token
 
-    if (previousStatus !== 'opened') {
-      console.log(`Opening stream for ${streamer}...`);
+  streamers.forEach((streamer) => {
+    console.log(`Opening stream for ${streamer}...`);
 
-      const token = await getAccessToken();  // Get Bearer token
+    const command =`xvfb-run --auto-servernum google-chrome-stable --new-window --disable-gpu --no-sandbox --mute-audio "https://www.twitch.tv/${streamer}?token=${token}"`
 
-      // For now, we don't need to check if the streamer is live since you wanted to always keep streams open
-      const command = OS === 'linux'
-        ? `DISPLAY=:99 google-chrome-stable --new-window --disable-gpu --no-sandbox --mute-audio "https://www.twitch.tv/${streamer}?token=${token}"`
-        : `start chrome "https://www.twitch.tv/${streamer}?token=${token}"`; // Windows command
-
-      exec(command, (err) => {
-        if (err) {
-          console.error(`Failed to open ${streamer}:`, err);
-        } else {
-          fs.writeFileSync(statusFile, 'opened');
-        }
-      });
-    }
+    exec(command, (err) => {
+      if (err) {
+        console.error(`Failed to open ${streamer}:`, err);
+      } else {
+        console.log(`Stream opened: ${streamer}`);
+      }
+    });
   });
 }
 
-// Run script at startup and repeat every 15 minutes
-(() => {
+// Run script at startup
+(async () => {
   console.log('Starting Twitch Farming Bot...');
-  openBrowserOnStreams();
+  await openBrowserOnStreams();
 })();
